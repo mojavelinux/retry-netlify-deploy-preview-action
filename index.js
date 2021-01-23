fd61ffab3698966bf409776ec70af903435dc09d
Dan Allen <dan.j.allen@gmail.com>
2021-01-23 04:19:28 -0700

    upgrade netlify client and switch to listSiteDeploys operation

--- index.js
+++ index.js
@@ -38,8 +38,7 @@ async function findDeployForPullRequest (client, siteID, pullRequestID) {
   let page = 1
   const marker = `https://deploy-preview-${pullRequestID}--`
   while (deploy == null) {
-    const response = await fetch(`https://api.netlify.com/api/v1/sites/${siteID}/deploys?per_page=15&page=${page++}`, { method: 'GET', headers: client.defaultHeaders })
-    const deploys = JSON.parse(await response.text())
+    const deploys = await client.listSiteDeploys({ site_id: siteID, per_page: 15, page: page++ })
     if (!deploys.length) break
     deploy = deploys.find(({ deploy_ssl_url }) => deploy_ssl_url.startsWith(marker))
   }
